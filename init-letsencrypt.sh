33d53127806b4621793efac6ca613b693ef773cd
Vorapas <max.saparov@gmail.com>
2022-05-22 16:46:53 -0400

    certbot bug fixes

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -82,7 +82,7 @@ esac
 if [ $staging != "0" ]; then staging_arg="--staging"; fi
 
 docker-compose run --rm --entrypoint "\
-  certbot certonly --webroot -w /var/www/certbot \
+  certbot certonly --webroot -w /var/www \
     $staging_arg \
     $email_arg \
     $domain_args \
