102bb560af421fe6fc56a9a2ce8ed9ec3d71f849
Vorapas <max.saparov@gmail.com>
2022-05-22 23:19:43 -0400

    init script

--- init.sh
+++ init.sh
@@ -1,17 +1,34 @@
 #!/bin/bash
 
-while getopts d:s: flag
+print_help() {
+  printf "Usage: ./init [-d protolemon.com] [-e email@email.com] [-s] \n"
+  printf "\t -d: main domain name \n\t -e: email for certbot \n\t -s: boolean flag for certbot staging"
+  echo
+  exit 1
+}
+
+domain="protolemon.com"
+staging=""
+email="zogz.max@gmail.com"
+
+while getopts d:e:sh flag
 do
     case "${flag}" in
-        d) domains=${OPTARG};;
-        s) staging="1";;
-        e) email=${OPTARG}
+        d) domain=${OPTARG};;
+        e) email=${OPTARG};;
+        s) staging="--staging";;
+        h) print_help;;
+        *) print_help;;
     esac
 done
-if [ -z "$domains" ]; then domains="www.example.protolemon.com example.protolemon.com"; fi
-if [ -z "$staging" ]; then staging="0"; fi
-if [ -z "$email" ]; then email="zogz.max@gmail.com"; fi
 
+read -p "### Enter a new subdomain name: " subdomain  
+
+domains="${subdomain}.${domain} www.${subdomain}.${domain}"
+darrays=(${domains})
+d_domains=("${darrays[*]/#/-d }") 
+
+echo "### Initializing temporary nginx container..."
 docker build nginx -f nginx/Dockerfile-init -t nginx-temp
 
 docker run --rm -d --name nginx-temp \
@@ -20,14 +37,38 @@ docker run --rm -d --name nginx-temp \
             -p 80:80 \
             nginx-temp
 
-docker run --rm --name temp_certbot \
+
+echo "### Running certbot..."
+docker run --rm --name certbot-temp \
     -v "$(pwd)"/data/certbot/letsencrypt:/etc/letsencrypt \
     -v certbot_challenges:/var/www/html \
     certbot/certbot \
-    certonly --webroot --agree-tos --renew-by-default \
+    certonly --webroot --text --agree-tos --renew-by-default \
+    --keep-until-expiring \
     --preferred-challenges http-01 --server https://acme-v02.api.letsencrypt.org/directory \
-    --text --email zogz.max@gmail.com \
-    --test-cert \
-    -w /var/www/html -d example.protolemon.com
+    --text --email ${email} ${staging} \
+    -w /var/www/html ${d_domains}
 
+echo "### Shutting down temporary nginx container... "
 docker stop nginx-temp
+
+echo "### Writing crontab to auto update certbot... "
+crontab -l > temp_crontab
+
+echo "0 0 * * * \
+docker run --rm --name certbot-temp \
+-v "$(pwd)"/data/certbot/letsencrypt:/etc/letsencrypt \
+-v certbot_challenges:/var/www/html \
+certbot/certbot \
+certonly --webroot --text --agree-tos --renew-by-default \
+--keep-until-expiring \
+--preferred-challenges http-01 --server https://acme-v02.api.letsencrypt.org/directory \
+--text --email ${email} ${staging} \
+-w /var/www/html ${d_domains}" >> temp_crontab
+
+echo "1 0 * * * docker-compose exec nginx nginx -s reload" >> temp_crontab
+
+crontab temp_crontab
+rm temp_crontab
+
+exit 0
