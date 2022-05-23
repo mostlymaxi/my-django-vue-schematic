29d0d463dccc369ee5bc52e7994339b7ce40f64e
Vorapas <max.saparov@gmail.com>
2022-05-22 20:37:12 -0400

    certbot bug fixes

--- init.sh
+++ init.sh
@@ -13,10 +13,12 @@ if [ -z "$staging" ]; then staging="0"; fi
 if [ -z "$email" ]; then email="zogz.max@gmail.com"; fi
 
 docker build nginx -f nginx/Dockerfile-init -t nginx-temp
+
 docker run -d --name nginx-temp \
             -v "$pwd/data/certbot/letsencrypt:/etc/letsencrypt" \
             -v "$pwd/data/certbot/www:/var/www" \
             nginx-temp
+
 docker run --rm --name temp_certbot \
     -v "$pwd/data/certbot/letsencrypt:/etc/letsencrypt" \
     -v "$pwd/data/certbot/www:/var/www" \
@@ -24,4 +26,5 @@ docker run --rm --name temp_certbot \
     certonly --webroot --agree-tos --renew-by-default \
     --preferred-challenges http-01 --server https://acme-v02.api.letsencrypt.org/directory \
     --text --email zogz.max@gmail.com \
-    -w /tmp/letsencrypt -d example.protolemon.com
+    --test-cert \
+    -w /var/www -d example.protolemon.com
