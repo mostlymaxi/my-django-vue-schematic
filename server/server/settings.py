8ac2215647f2f621834e66851aefea82f2a022ef
Vorapas <max.saparov@gmail.com>
2022-05-29 23:28:37 -0400

    settings

--- server/server/settings.py
+++ server/server/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = '/static/'
 STATIC_CLIENT = os.path.join(os.path.dirname(BASE_DIR), 'static')
 STATIC_URL = '/static/'
 
